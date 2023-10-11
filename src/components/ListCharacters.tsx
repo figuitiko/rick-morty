fa6211436cb01f9ee5432a1f4a41a9e258f978fe
figuitiko <frankfigao@gmail.com>
2023-10-10 19:39:01 -0600

    Adding rounded box

--- src/components/ListCharacters.tsx
+++ src/components/ListCharacters.tsx
@@ -15,10 +15,11 @@ type ItemPropType = Pick<CharacterProps, 'image' | 'name'>
 
 export const CharacterBox = ({ image, name }: ItemPropType) => {
   return (
-    <div className='flex flex-col w-full sm:w-1/2 md:w-1/4  lg:w-1/5'>
+    <div className='flex flex-col w-full sm:w-1/2 md:w-1/4  lg:w-1/5 '>
       <picture>
         <source type='image/jpeg' srcSet={`${image}?width=100 100w`} />
         <img
+         className='rounded-lg shadow-lg'
           role='presentation'
           loading='lazy'
           src={image}
