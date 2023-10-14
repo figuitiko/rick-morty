a6342a3ad4d2c0fdaf2e5ba276269d58ecd0f761
figuitiko <frankfigao@gmail.com>
2023-10-13 21:16:20 -0600

    Adding await render props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,32 @@
 import { ListCharacters } from '@/components/ListCharacters'
+import Await from '@/components/share/Await'
+import Skeleton from '@/components/share/Skeleton'
 import { fetchCharacters } from '@/lib/characters'
+import { Suspense } from 'react'
 
 type ISearchParms = Record<string, string | string | string[] | undefined>
 
 export default async function Home ({ searchParams }: { searchParams: ISearchParms }) {
   const page =
     typeof searchParams.page === 'string' ? Number(searchParams.page) : 1
-  const data = await fetchCharacters({ page })
-  const { info, characters } = data
-  const { pages } = info
-  const pagesArr = pages !== undefined ? Array.from(Array(pages).keys()) : []
+
   return (
     <main className='flex min-h-screen flex-col justify-center items-center p-24'>
-      <ListCharacters characters={characters} pages={pagesArr} currentPage={page} />
+      <Suspense fallback={<Skeleton />}>
+        <Await promise={fetchCharacters({ page })}>
+          {({ info, characters }) => {
+            const { pages } = info
+            const pagesArr = pages !== undefined ? Array.from(Array(pages).keys()) : []
+            return (
+              <ListCharacters
+                characters={characters}
+                pages={pagesArr}
+                currentPage={page}
+              />
+            )
+          }}
+        </Await>
+      </Suspense>
     </main>
   )
 }
